49b8996f3e8d1b37a22029e10d4a91009a45ba76
roshan <enter>
2024-11-01 12:43:38 +0530

    nth ch

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+   let span = document.createElement("span");
+   span.innerHTML = "\u00D7";
+   li.appendChild(span);
     }
+    inputBox.value = '';
+    saveData();
 }
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+inputBox.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+
+function saveData() {
+    // let tasks = listContainer.innerHTML;
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
 // console.log('Hello World');
